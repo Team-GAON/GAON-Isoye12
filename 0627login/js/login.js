c3e3552efc530b1b2db4ff3ae6259cdd9088a66b
Isoye12 <145185920+Isoye12@users.noreply.github.com>
2024-06-27 20:51:56 +0900

    loginHtml

---
+++ 0627login/js/login.js
@@ -0,0 +1,6 @@
+const login = () => {
+    const id = document.getElementById('logId')
+    const password = document.getElementById('logPassword')
+    console.log(id.value)
+    console.log(password.value)
+}
---
+++ 0627login/js/register.js
@@ -0,0 +1,8 @@
+const userInfo = {}
+
+const login = () => {
+    const id = document.getElementById('registerId');
+    const password = document.getElementById('registerPassword');
+    const phoneNumber = document.getElementById('registerPhoneNumber');
+    userId[id] = password, phoneNumber;
+};
